361785285d934ff5c10271e7bf641b0ef2eec9cd
Rob Ireton <rob@ireton.org>
2025-08-14 16:55:56 -0400

    build :latest and :dev images

--- build.sh
+++ build.sh
@@ -1,4 +1,18 @@
 #!/usr/bin/env bash
-NAME=`jq -r .name package.json | cut -d/ -f2`
-VERSION=`jq -r .version package.json`
-docker build --tag $NAME:$VERSION --tag $NAME:latest .
+
+NAME=$(jq -r .name package.json | cut -d/ -f2)
+VERSION=$(jq -r .version package.json)
+
+case "$1" in
+  release)
+    EXTRA_TAGS=(--tag "$NAME:latest")
+    ;;
+  dev)
+    EXTRA_TAGS=(--tag "$NAME:dev")
+    ;;
+  *)
+    EXTRA_TAGS=()
+    ;;
+esac
+
+docker build --tag "$NAME:$VERSION" "${EXTRA_TAGS[@]}" .
